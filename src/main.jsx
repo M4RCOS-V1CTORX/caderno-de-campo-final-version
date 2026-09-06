0241580602a863d3f0f041948197998f44f1d400
MARCOS SILVA <m4rcos.v1ctorx@gmail.com>
2026-09-06 07:31:17 -0300

    final-version-1.0

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,15 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => {
+        console.log('Service Worker registrado com sucesso.')
+      })
+      .catch((error) => {
+        console.error('Erro ao registrar o Service Worker:', error)
+      })
+  })
+}
